dc458dbfca94e9f81ead1558dff73cd0d4492bea
4geru <westhouse51@gmail.com>
2022-04-29 13:03:33 +0900

    separate theme color

---
+++ lib/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor anyaColor = MaterialColor(
+    0xFFFFCEE4,
+    <int, Color> {
+      50: Color(0xFFFFEBF4),
+      100: Color(0xFFFFCEE4),
+      200: Color(0xFFFFC2DD),
+      300: Color(0xFFFFADD2),
+      400: Color(0xFFFF99C7),
+      500: Color(0xFFFF85BC),
+      600: Color(0xFFFF70B0),
+      700: Color(0xFFFF5CA5),
+      800: Color(0xFFFF479A),
+      900: Color(0xFFFF338F),
+    }
+);
+
+const Color anyaSecondaryColor = Color(0xFFFF9BC8);
+const Color anyaTextColor = Color(0xFF0F1021);
+const Color anyaselectedColor = Color(0xFFFF338F);
+const Color anyaWhiteColor = Colors.white;
