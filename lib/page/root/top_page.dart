002e1881aca0c02124443c407e06f1839c86e117
4geru <westhouse51@gmail.com>
2022-05-14 18:03:03 +0900

    pagnate with some

--- lib/page/root/top_page.dart
+++ lib/page/root/top_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_study_day7/data/repo/history_provider.dart';
+import 'package:flutter_study_day7/model/simple_tv_object.dart';
 import 'package:flutter_study_day7/package/bubble_tab_indicator.dart';
 import 'package:flutter_study_day7/page/root/year_tab_page.dart';
 import 'package:flutter_study_day7/theme.dart';
+import 'package:provider/provider.dart';
 
 class TabInfo {
   int year;
@@ -32,7 +35,7 @@ class _TopHomePageState extends State<TopHomePage> with SingleTickerProviderStat
   late TabController _controller;
   final List<TabInfo> _tabs = tabsInfo;
   int _selectedYear = tabsInfo.last.year;
-  int selectedPage = 0;
+  int _selectedIndex = 0;
 
   @override
   void initState() {
@@ -53,51 +56,67 @@ class _TopHomePageState extends State<TopHomePage> with SingleTickerProviderStat
   @override
   Widget build(BuildContext context) {
     FlutterNativeSplash.remove();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'ANYA in ${_selectedYear.toString()}',
-          style: const TextStyle(
-            color: anyaTextColor,
-          ),
-        ),
-        bottom: TabBar(
-          isScrollable: true,
-          controller: _controller,
-          tabs: _tabs.map((TabInfo tab) {
-            return Tab(
-              text: tab.year.toString(),
-            );
-          }).toList(),
-          labelColor: const Color(0xFF0F1021),
-          indicatorSize: TabBarIndicatorSize.tab,
-          indicator: const BubbleTabIndicator(
-            indicatorHeight: 30.0,
-            indicatorColor: anyaWhiteColor,
-            tabBarIndicatorSize: TabBarIndicatorSize.tab,
-          ),
-        )
-      ),
-      body: TabBarView(
+    List<SimpleTvObject> histories = Provider.of<HistoryProvider>(context).histories.reversed.toList();
+    late Widget body;
+    if(_selectedIndex == 0 || _selectedIndex == 2) {
+      body = TabBarView(
         children: _tabs.map((tab) => tab.widget).toList(),
         controller: _controller,
+      );
+    } else {
+      body = ListView(
+        children: <Widget>[
+          Card(child: ListTile(title: Text('履歴一覧'))),
+          ...histories.map((e) {
+            return Card(
+              child: ListTile(
+                leading: Image.network(
+                  'https://image.tmdb.org/t/p/w300/${e.posterPath}',
+                ),
+                title: Text(e.originalName),
+              ),
+            );
+          })
+        ],
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+            'ANYA in ${_selectedYear.toString()}',
+            style: const TextStyle(
+              color: anyaTextColor,
+            ),
+          ),
+          bottom: TabBar(
+            isScrollable: true,
+            controller: _controller,
+            tabs: _tabs.map((TabInfo tab) {
+              return Tab(
+                text: tab.year.toString(),
+              );
+            }).toList(),
+            labelColor: const Color(0xFF0F1021),
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicator: const BubbleTabIndicator(
+              indicatorHeight: 30.0,
+              indicatorColor: anyaWhiteColor,
+              tabBarIndicatorSize: TabBarIndicatorSize.tab,
+            ),
+          )
       ),
+      body: body,
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home_sharp, size: 20), activeIcon: Icon(Icons.home_rounded, size: 30), label: 'ホーム'),
           BottomNavigationBarItem(icon: Icon(Icons.history_sharp, size: 20), activeIcon: Icon(Icons.history_rounded, size: 30), label: '履歴'),
           BottomNavigationBarItem(icon: Icon(Icons.favorite_sharp, size: 20), activeIcon: Icon(Icons.favorite_rounded, size: 30), label: 'お気に入り'),
         ],
         elevation: 5.0,
-        currentIndex: selectedPage,
         onTap: (index){
           setState(() {
-            selectedPage = index;
-            if(index == 1) {
-              Navigator.restorablePushNamed(context, '/history');
-            } else {
-              selectedPage = index;
-            }
+            _selectedIndex = index;
           });
         },
       )
