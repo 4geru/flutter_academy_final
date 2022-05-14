849e49dea32639eb15b760586f58ee81c2dead76
4geru <westhouse51@gmail.com>
2022-05-14 20:53:28 +0900

    connect to link between history and detail

--- lib/page/root/components/history_tab.dart
+++ lib/page/root/components/history_tab.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_study_day7/data/repo/history_provider.dart';
 import 'package:flutter_study_day7/model/simple_tv_object.dart';
+import 'package:flutter_study_day7/page/details/detail_page.dart';
+import 'package:flutter_study_day7/page/details/detail_page_argument.dart';
+import 'package:flutter_study_day7/page/details/hooks.dart';
 import 'package:flutter_study_day7/page/root/components/base_tab.dart';
 import 'package:flutter_study_day7/theme.dart';
+import 'package:provider/provider.dart';
 
 class HistoryTab extends BaseTab {
-  List<SimpleTvObject> histories;
-  HistoryTab({required this.histories});
+  BuildContext context;
+  int selectedYear;
+  HistoryTab({required this.context, required this.selectedYear});
 
   PreferredSizeWidget appBar() {
     return AppBar(
@@ -19,16 +25,34 @@ class HistoryTab extends BaseTab {
   }
 
   Widget body() {
+    final store = context.watch<UseDetailPage>();
+    List<SimpleTvObject> histories = Provider.of<HistoryProvider>(context).histories.reversed.toList();
     return ListView(
       children: <Widget>[
         const Card(child: ListTile(title: Text('履歴一覧'))),
-        ...histories.map((e) {
+        ...histories.map((history) {
           return Card(
             child: ListTile(
               leading: Image.network(
-                'https://image.tmdb.org/t/p/w300/${e.posterPath}',
+                'https://image.tmdb.org/t/p/w300/${history.posterPath}',
               ),
-              title: Text(e.originalName),
+              title: Text(history.originalName),
+              onTap: () {
+                store.fetch(history.id);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    settings: const RouteSettings(name: "/details/:id"),
+                    builder: (BuildContext context) => DetailPage(
+                      argument: DetailPageArgument(
+                        tvId: history.id,
+                        year: selectedYear
+                      )
+                    ),
+                    fullscreenDialog: true,
+                  ),
+                );
+              },
             ),
           );
         })
