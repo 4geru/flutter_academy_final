c559edd88cc17c6e17f622394feaf7984b7b7b31
4geru <westhouse51@gmail.com>
2022-05-14 21:54:59 +0900

    change dropdown component for seasons

--- lib/page/details/detail_page.dart
+++ lib/page/details/detail_page.dart
@@ -21,6 +21,7 @@ class DetailPage extends StatefulWidget {
 
 class _DetailPageState extends State<DetailPage> {
   late UseDetailPage useDetailPage;
+  bool _openSeason = false;
 
   @override
   Widget build(BuildContext context) {
@@ -39,29 +40,39 @@ class _DetailPageState extends State<DetailPage> {
             TitleDurationAndFabBtn(tvDetailResultObject: store.tvDetailResultObject!),
             Genres(genres: store.tvDetailResultObject?.genres ?? []),
             if(store.tvDetailResultObject?.overview != '') Overview(overview: store.tvDetailResultObject?.overview ?? ""),
-            const Card(child: ListTile(title: Text('Season'))),
-            ...(store.tvDetailResultObject?.seasons ?? []).map((e) {
-              String openDate = "${e.airDate?.year}年${e.airDate?.month}月${e.airDate?.day}日";
-              return Card(
-                child: ListTile(
-                  leading: Image.network(
-                    'https://image.tmdb.org/t/p/w300/${e.posterPath}',
+            Card(
+              child: ListTile(
+                title: const Text('シーズン'),
+                trailing: _openSeason ? Icon(Icons.arrow_drop_up) : Icon(Icons.arrow_drop_down),
+                onTap: () => setState(() =>_openSeason = !_openSeason)
+              ),
+            ),
+            if(_openSeason)
+              ...(store.tvDetailResultObject?.seasons ?? []).map((e) {
+                String openDate = "${e.airDate?.year}年${e.airDate?.month}月${e.airDate?.day}日";
+                return Card(
+                  child: ListTile(
+                    leading: Image.network(
+                      'https://image.tmdb.org/t/p/w300/${e.posterPath}',
+                    ),
+                    title: Text(e.name),
+                    subtitle: Row(
+                      children: [
+                        if(
+                          widget.argument.year == e.airDate?.year ||
+                          (e.airDate == null && widget.argument.year != e.airDate?.year)
+                        ) const Icon(
+                          Icons.favorite,
+                          color: anyaSecondaryColor,
+                          size: 24.0,
+                          semanticLabel: 'Text to announce in accessibility modes',
+                        ),
+                        Text("${e.airDate != null ? openDate : ''}${e.episodeCount}話"),
+                      ]
+                    )
                   ),
-                  title: Text(e.name),
-                  subtitle: Row(
-                    children: [
-                      if(widget.argument.year == e.airDate?.year) const Icon(
-                        Icons.favorite,
-                        color: anyaSecondaryColor,
-                        size: 24.0,
-                        semanticLabel: 'Text to announce in accessibility modes',
-                      ),
-                      Text("${e.airDate != null ? openDate : ''}${e.episodeCount}話"),
-                    ]
-                  )
-                ),
-              );
-            }),
+                );
+              }),
             CastAndCrew(casts: store.aggregateCreditObject!.casts ?? []),
           ],
         )
