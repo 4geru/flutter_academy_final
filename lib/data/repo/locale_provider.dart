e666835d7685abee96c2e6db5ac889b5c6b43f3c
4geru <westhouse51@gmail.com>
2022-05-25 06:57:10 +0900

    implement locale_provider

---
+++ lib/data/repo/locale_provider.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations_en.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations_ja.dart';
+
+final Map<String, AppLocalizations> Locales = {
+  'en': AppLocalizationsEn(),
+  'ja': AppLocalizationsJa()
+};
+
+class LocaleProvider with ChangeNotifier {
+  Locale _locale = Locale('en', '');
+  Locale? get locale => _locale;
+
+  Map<String, AppLocalizations> languages = {
+    'en': AppLocalizationsEn(),
+    'ja': AppLocalizationsJa()
+  };
+
+  List<Locale> all() {
+    return [
+      const Locale('en', ''),
+      const Locale('ja', ''),
+    ];
+  }
+
+  void setLocale(Locale locale) {
+    if (!all().contains(locale)) return;
+    _locale = locale;
+    notifyListeners();
+  }
+
+  AppLocalizations load() =>
+      languages[_locale.languageCode] ?? AppLocalizationsEn();
+}
--- lib/di_container.dart
+++ lib/di_container.dart
@@ -2,12 +2,14 @@ import 'package:flutter_study_day7/data/repo/theme_provider.dart';
 import 'package:get_it/get_it.dart';
 
 import 'data/repo/history_provider.dart';
+import 'data/repo/locale_provider.dart';
 import 'page/details/hooks.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
   sl.registerFactory(() => ThemeProvider());
-  sl.registerFactory(() => UseDetailPage());
   sl.registerFactory(() => HistoryProvider());
+  sl.registerFactory(() => LocaleProvider());
+  sl.registerFactory(() => UseDetailPage());
 }
