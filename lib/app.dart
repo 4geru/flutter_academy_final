90c58d777dd5700315f205bd976727f787df2ae9
4geru <westhouse51@gmail.com>
2022-05-25 08:45:20 +0900

    fix some

--- lib/app.dart
+++ lib/app.dart
@@ -19,7 +19,7 @@ class _AppState extends State<App> {
     Provider.of<ThemeProvider>(context).loadTheme();
     return MaterialApp(
       title: 'Flutter Demo',
-      localizationsDelegates: [
+      localizationsDelegates: const [
         AppLocalizations.delegate,
       ],
       supportedLocales: const [
