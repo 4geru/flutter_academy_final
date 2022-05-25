c20683aeed63ceb2cd9a82b526c80a29bd03573f
4geru <westhouse51@gmail.com>
2022-05-26 07:39:55 +0900

    translate detail page

--- lib/page/details/components/title_duration_and_fav_btn.dart
+++ lib/page/details/components/title_duration_and_fav_btn.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../../data/repo/locale_provider.dart';
 import '../../../model/tv_detail_result_object.dart';
 import '../../../theme.dart';
 
@@ -12,6 +14,7 @@ class TitleDurationAndFabBtn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = Provider.of<LocaleProvider>(context).load();
     return Padding(
       padding: const EdgeInsets.all(anyaDefaultPadding),
       child: Row(
@@ -27,11 +30,12 @@ class TitleDurationAndFabBtn extends StatelessWidget {
                 const SizedBox(height: anyaDefaultPadding / 2),
                 Row(
                   children: <Widget>[
+                    Text('${l10n.detail_start_date} '),
                     Text(
-                        '${tvDetailResultObject.firstAirDate.year}年${tvDetailResultObject.firstAirDate.month}月${tvDetailResultObject.firstAirDate.day}日'),
+                        '${tvDetailResultObject.firstAirDate.year}/${tvDetailResultObject.firstAirDate.month}/${tvDetailResultObject.firstAirDate.day}'),
                     const SizedBox(width: anyaDefaultPadding),
                     Text(
-                        '${tvDetailResultObject.numberOfSeasons}シーズン ${tvDetailResultObject.numberOfEpisodes}話'),
+                        '${tvDetailResultObject.numberOfSeasons}${l10n.detail_season} ${tvDetailResultObject.numberOfEpisodes}${l10n.story_count}'),
                   ],
                 )
               ],
